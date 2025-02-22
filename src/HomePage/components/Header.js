535ed392da34d1bd5baeb3dd51df724e5a108489
madiha-sumbal <133386903+madiha-sumbal@users.noreply.github.com>
2025-02-22 17:57:34 +0500

    Update Header.js

--- src/HomePage/components/Header.js
+++ src/HomePage/components/Header.js
@@ -1,1 +1,25 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav className="header">
+      <div className="logo">
+        {/* <img src="logo.png" alt="Exytex Logo" className="logo-img" /> */}
+        <span className="logo-text">TechTwirl</span>
+      </div>
+      <ul className="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Team</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <button className="hire-button">Hire Us</button>
+    </nav>
+  );
+};
+
+export default Header;
 
