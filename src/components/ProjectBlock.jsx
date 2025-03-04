e4280070be2e3614b5ac223a98d61d2712bcb1d9
Manahil Habib <manahil5503@gmail.com>
2025-03-04 21:04:13 +0500

    Update ProjectBlock.jsx

--- src/components/ProjectBlock.jsx
+++ src/components/ProjectBlock.jsx
@@ -4,7 +4,7 @@ import "../styles/ProjectBlock.css";
 const ProjectBlock = ({ heading, text, image }) => {
   return (
     <div className="projectCard">
-      {image && <img src={image} alt={heading} className="projectImage" />}
+      <img src={image[0]} alt="Project" className="projectImage" />
       <h2 className="projectTitle">{heading}</h2>
       <p className="projectSubtitle">{text}</p>
     </div>
