f73b55353f4c525188e3ccf0ccbc7b0fd06a0675
Manahil Habib <manahil5503@gmail.com>
2025-03-06 21:14:22 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/SideBar';
 import Header from './components/Header';
 import HireUs from './pages/hireUs';
 import PortfolioPage from './pages/portfolio';
+import Footer from './components/Footer';
 function App() {
   return (
-    <div className="">
-     {/* <Sidebar/>
-     <Header/> */}
-     <PortfolioPage/>
-    </div>
+    <Router>
+    <Header />
+    <Routes>
+      <Route path="/" element={<PortfolioPage/>} />
+      <Route path="/hire-us" element={<HireUs />} />
+    </Routes>
+    <Footer />
+  </Router>
   );
 }
 
