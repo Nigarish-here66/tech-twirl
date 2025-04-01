d8caf2d3360848f16718f2daf30a648dea8bbabe
Madiha Abbasi <133386903+madiha-sumbal@users.noreply.github.com>
2025-04-01 13:44:48 +0500

    Create Card1.jsx

---
+++ src/pages/HomePage/Card1.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "../styles/Card1.css";
+import serviceImage from "../assests/mobile-app.png";
+
+const Card1 = () => {
+  return (
+    <div className="service-container">
+      <div className="service-image-container">
+        <img 
+          src={serviceImage} 
+          alt="Software Development Services" 
+          className="service-image" 
+        />
+      </div>
+      <div className="service-content">
+        <h2>
+        Top Software Application Development</h2>
+        <p className="service-text">
+        API Development, and IOS app development in one place, 
+        without loss of quality and the difficult path of selecting and 
+        changing contractors of SEO Company Pakistan. If your company needs a 
+        new website, a new online store design or an application, 
+        you have found the right company. At Exytex Technologies we are 
+        specialists in programming. We carry out developments of any size 
+        and adapted to your needs and budget. In addition, our experience 
+        in Digital marketing services is very great advantage for our clients,
+ since all our projects are oriented to online success.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card1;
