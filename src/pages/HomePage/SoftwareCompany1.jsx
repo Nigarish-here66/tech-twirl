b7a00bba023b8c89557bae8f70af10d23ab45df5
Madiha Abbasi <133386903+madiha-sumbal@users.noreply.github.com>
2025-04-01 13:46:14 +0500

    Create SoftwareCompany1.jsx

---
+++ src/pages/HomePage/SoftwareCompany1.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "../styles/SoftwareCompany1.css";
+
+
+const SoftwareCompany1 = () => {
+  return (
+    <div className="service-container">
+      <div className="service-image-container">
+        <img 
+          src="https://www.exytex.com/assets/img/lastnew2.jpg" 
+          alt="Software Development Services" 
+          className="service-image" 
+        />
+      </div>
+      <div className="service-content">
+        <h2>Bespoke Software for Business</h2>
+        <p className="service-text">
+        
+        Talking about the consideration of Software companies, 
+        we should be aware about the deviation between software
+         houses and software companies. Mostly the software companies 
+         do have the software houses, however, sometimes they have 
+         different identification. There are many software companies 
+         which are working in UK and Pakistan. Most of them get the highest 
+         rating by their superior work. Looking at the list of top software
+          companies, Exytex invade in the race of best companies and got 5.00 
+          rating. This rating clearly shows the superiority of TechTwirl in the
+           arena of best software services. The reasons behind it are, zeal, 
+           passion, fair work, keenness, enthusiastic teamwork, faith and long 
+           lasting approach.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SoftwareCompany1;
