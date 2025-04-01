51ef50101e325d3642a2bcbf68c8efcd1fd10249
Madiha Abbasi <133386903+madiha-sumbal@users.noreply.github.com>
2025-04-01 13:45:49 +0500

    Create SoftwareCompany.jsx

---
+++ src/pages/HomePage/SoftwareCompany.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "../styles/Card.css";
+
+const SoftwareCompany = () => {
+  return (
+    <div className="service-container">
+      <div className="service-content">
+        <h2>A Software Company Serving the Worldwide</h2>
+        <p className="service-text">
+        Whenever we are casting about any software house, 
+        the only thing comes first in our minds is on time and 
+        satisfied work. Plenty of software houses work for providing software
+         development services, however, only few of them sustain their reputation. 
+         TechTwirl Technologies is one of them. TechTwirl have in house development 
+         teams and also an office development team. Workers do the project
+          with full enthusiasm and determination. The supreme motto of techtwirl 
+          is to provide non-pareil results. TechTwirl is the registered Software firm, 
+          where any client from all over the world can attain their required work. 
+          TechTwirl got their highest ranks in very less time.
+        </p>
+      </div>
+      <div className="service-image-container">
+        <img 
+          src="https://www.exytex.com/assets/img/softnew2.jpg" 
+          alt="Software Development Services" 
+          className="service-image"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SoftwareCompany;
