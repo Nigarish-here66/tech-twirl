6c838fbc5acfb92553c041f5c075c48041faccab
Madiha Abbasi <133386903+madiha-sumbal@users.noreply.github.com>
2025-04-06 13:32:57 +0500

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import AboutSection from './AboutSection';
+import Card from './Card';
+import Card1 from './Card1';
+import HeroSection from './HeroSection';
+import HireUS from './HireUS';
+import OurServices from './OurServices';
+import SoftwareComapany from './SoftwareComapany';
+import SoftwareCompany1 from './SoftwareCompany1';
+import Stats from './Stats';
+
+export {
+  AboutSection,
+  Card,
+  Card1,
+  HeroSection,
+  HireUS,
+  OurServices,
+  SoftwareComapany,
+  SoftwareCompany1,
+  Stats,
+};
