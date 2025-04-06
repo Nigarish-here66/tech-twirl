310d8ed068f37f66005e13ed7223e3541e697b19
Madiha Abbasi <133386903+madiha-sumbal@users.noreply.github.com>
2025-04-06 13:42:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,31 @@ import Header from './components/Header';
 import HireUs from './pages/hireUs';
 import PortfolioPage from './pages/portfolio';
 import Footer from './components/Footer';
+import {
+  AboutSection,
+  Card,
+  Card1,
+  HeroSection,
+  HireUS,
+  OurServices,
+  SoftwareComapany,
+  SoftwareCompany1,
+  Stats,
+} from './Pages'; 
 function App() {
   return (
+    <HeroSection />
+      <AboutSection />
+      <OurServices />
+      <SoftwareComapany/>
+      <SoftwareCompany1/>
+      <HireUS/>
+      <Card/>
+      <Card1/>
+      <Stats/>
+    
+      <SoftwareCompany1/>
+     
     <Router>
     <Header />
     <Routes>
