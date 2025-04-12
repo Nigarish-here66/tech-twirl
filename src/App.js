5ad647c584445bc4eaa2cc1bad37e3a8d93f54f9
Manahil Habib <manahil5503@gmail.com>
2025-04-13 00:04:13 +0500

    scroll to top issue fixed App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
@@ -6,18 +7,21 @@ import Footer from './components/Footer';
 import Homepage from './HomePage';
 import OurServices from './pages/services';
 import Contact from './pages/contact';
+import ScrollToTop from './components/ScrollToTop'; 
+
 function App() {
   return (
     <Router>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/services" element={<OurServices />} />
-      <Route path="/portfolio" element={<PortfolioPage />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-    <Footer />
-  </Router>
+      <ScrollToTop />  {/* This ensures that scroll position resets on route change */}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/services" element={<OurServices />} />
+        <Route path="/portfolio" element={<PortfolioPage />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
