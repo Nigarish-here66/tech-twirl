2cf398dadad4fd706e4be6d0a7be0608c3536cf9
Madiha Abbasi <133386903+madiha-sumbal@users.noreply.github.com>
2025-04-24 20:47:46 +0500

    Create contactRoutes.js

---
+++ Backend/routes/contactRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const Contact = require('../models/contact');
+
+// CREATE
+router.post('/', async (req, res) => {
+  try {
+    console.log("Request Body:", req.body); 
+    const contactRequest = new Contact(req.body);
+    await contactRequest.save();
+    res.status(201).json(contactRequest);
+  } catch (err) {
+    console.error("Error during save:", err.message); 
+    res.status(400).json({ error: err.message });
+  }
+});
+
+
+// READ ALL
+router.get('/', async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    res.json(contacts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
